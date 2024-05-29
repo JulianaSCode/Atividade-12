83ca4e0bbcffda87d4365dd9a19b181e17e9d342
Juliana da Costa Silva <julianadacossta@gmail.com>
2024-05-28 21:50:53 -0300

    Fazendo alterações na branch

--- Teste1.c
+++ Teste1.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
 // Função para somar dois números
+
 float somar(float a, float b) {
     return a + b;
+}
+// Função para multiplicar dois números
+float multiplicar(float a, float b) {
+    return a * b;
 }
 
 int main() {
@@ -21,7 +26,11 @@ int main() {
     // Exibe o resultado da soma
     printf("O resultado da soma é: %.2f\n", resultado);
 
+    // Realiza a multiplicação dos números
+    resultado = multiplicar(num1, num2);
+
+    // Exibe o resultado da multiplicação
+    printf("O resultado da multiplicação é: %.2f\n", resultado);
+
     return 0;
 }
-
-
